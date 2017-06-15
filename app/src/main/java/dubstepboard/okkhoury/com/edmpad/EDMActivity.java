32e09153e900d2c1eb769e949df2066736df99c2
zskemp <zrskemp@gmail.com>
2017-06-14 22:08:19 -0400

    refactor to main activity

--- app/src/main/java/dubstepboard/okkhoury/com/edmpad/EDMActivity.java
+++ app/src/main/java/dubstepboard/okkhoury/com/edmpad/EDMActivity.java
@@ -1,30 +1,99 @@
 package dubstepboard.okkhoury.com.edmpad;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class EDMActivity extends FragmentActivity {
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class EDMActivity extends FragmentActivity implements AdapterView.OnItemSelectedListener {
+
+    Button loop1;
+    Button loop2;
+    Button loop3;
+
+    private static Context context;
+
+    // Set up the looping sound pools
+    private static SoundPool spLoop1 = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+    private static SoundPool spLoop2 = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+    private static SoundPool spLoop3 = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+
+    //Boolean variables to tell if a loop is being recorded
+    private static boolean loop1IsRecording;
+    private static boolean loop2IsRecording;
+    private static boolean loop3IsRecording;
+
+    //Queues to store the sounds in the loops
+    private Queue<Integer> loop1SoundQueue = new ArrayBlockingQueue<Integer>(1000);
+    private Queue<Integer> loop2SoundQueue = new ArrayBlockingQueue<Integer>(1000);
+    private Queue<Integer> loop3SoundQueue = new ArrayBlockingQueue<Integer>(1000);
+
+    //Queues to store the pauses between sounds in the loops
+    private static Queue<Long> loop1PauseQueue = new ArrayBlockingQueue<Long>(1000);
+    private static Queue<Long> loop2PauseQueue = new ArrayBlockingQueue<Long>(1000);
+    private static Queue<Long> loop3PauseQueue = new ArrayBlockingQueue<Long>(1000);
+
+    // number to store the time at which the previous sound finsihed playing (used for loops)
+    private static long loop1EndOfLastSound;
+    private static long loop2EndOfLastSound;
+    private static long loop3EndOfLastSound;
+
+//    //Async tasks for playing loops in the background
+//    private loopingTask loopTask = new loopingTask();
+//    private loopingTask2 loopTask2 = new loopingTask2();
+//    private loopingTask3 loopTask3 = new loopingTask3();
+
+    // Keep track of which loops are currently being played
+    private boolean loop1IsOn = false;
+    private boolean loop2IsOn = false;
+    private boolean loop3IsOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edm);
 
+        context = getApplicationContext();
+
         // Prevent screen from getting dim
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_dropdown);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.dropdown_list, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
@@ -35,6 +104,116 @@ public class EDMActivity extends FragmentActivity {
                     .add(R.id.fragmentContainer, fragment)
                     .commit();
         }
+
+        // Set up the looping sound pools with on load listeners
+        setUpLoopSP(spLoop1, loop1SoundQueue);
+        setUpLoopSP(spLoop2, loop2SoundQueue);
+        setUpLoopSP(spLoop3, loop3SoundQueue);
+
+        loop1 = (Button) findViewById(R.id.loop1);
+        loop1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loop1.getText().equals("Loop 1")) {
+                    loop1.setText("Stop");
+                    loop1EndOfLastSound = System.currentTimeMillis();
+                    loop1IsRecording = true;
+                    //return true;
+                } else if (loop1.getText().equals("Stop")) {
+                    loop1.setText("End");
+                    // Add the time from last sound pressed to clicking stop
+                    // Delete the first element from the queue.
+                    // Don't want to include time from first press of loop to first sound press
+                    loop1PauseQueue.add(System.currentTimeMillis() - loop1EndOfLastSound);
+                    loop1PauseQueue.remove();
+
+                    loop1IsOn = true;
+
+                    loop1IsRecording = false;
+                    //loopTask = new loopingTask();
+                    //loopTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
+
+                    runLoop1();
+                    //return true;
+                } else if (loop1.getText().equals("End")) {
+                    loop1.setText("Loop 1");
+
+                    loop1IsOn = false;
+                    loop1SoundQueue.clear();
+                    loop1PauseQueue.clear();
+                    //loopTask.cancel(true);
+                    //return true;
+                }
+            }
+        });
+        loop2 = (Button) findViewById(R.id.loop2);
+        loop2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loop2.getText().equals("Loop 2")) {
+                    loop2.setText("Stop");
+                    loop2EndOfLastSound = System.currentTimeMillis();
+                    loop2IsRecording = true;
+                    //return true;
+                } else if (loop2.getText().equals("Stop")) {
+                    loop2.setText("End");
+
+                    loop2IsOn = true;
+
+                    loop2PauseQueue.add(System.currentTimeMillis() - loop2EndOfLastSound);
+                    loop2PauseQueue.remove();
+
+                    loop2IsRecording = false;
+
+                    runLoop2();
+
+                    //return true;
+                } else if (loop2.getText().equals("End")) {
+                    loop2.setText("Loop 2");
+
+                    loop2IsOn = false;
+
+                    loop2SoundQueue.clear();
+                    loop2PauseQueue.clear();
+
+                    //return true;
+                }
+            }
+        });
+        loop3 = (Button) findViewById(R.id.loop3);
+        loop3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loop3.getText().equals("Loop 3")) {
+                    loop3.setText("Stop");
+                    loop3EndOfLastSound = System.currentTimeMillis();
+                    loop3IsRecording = true;
+                    //return true;
+                } else if (loop3.getText().equals("Stop")) {
+                    loop3.setText("End");
+
+                    loop3IsOn = true;
+
+                    loop3PauseQueue.add(System.currentTimeMillis() - loop3EndOfLastSound);
+                    loop3PauseQueue.remove();
+
+                    loop3IsRecording = false;
+
+                    runLoop3();
+
+                   // return true;
+                } else if (loop3.getText().equals("End")) {
+                    loop3.setText("Loop 3");
+
+                    loop3IsOn = false;
+
+                    loop3SoundQueue.clear();
+                    loop3PauseQueue.clear();
+
+                    //return true;
+                }
+            }
+        });
 
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -49,6 +228,50 @@ public class EDMActivity extends FragmentActivity {
 //        });
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        Log.i("Here", "I got here");
+        Fragment fragment = null;
+        switch (pos) {
+            case 0:
+                releaseEverything();
+                fragment = new EDMFragment1(); // Ethereal
+                break;
+            case 1:
+                releaseEverything();
+                fragment = new EDMFragment2(); // Ethereal
+                break;
+            case 2:
+                releaseEverything();
+                fragment = new EDMFragment3(); // Ethereal
+                break;
+            case 3:
+                releaseEverything();
+                fragment = new EDMFragment4(); // Ethereal
+                break;
+            case 4:
+                releaseEverything();
+                fragment = new EDMFragment5(); // Ethereal
+                break;
+            default:
+                releaseEverything();
+                fragment = new EDMFragment1(); // Ethereal
+                break;
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
@@ -56,21 +279,212 @@ public class EDMActivity extends FragmentActivity {
 //        return true;
 //    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    // Fill in the sound and pause for the respective queues
+    public static void handleLoopingQueues(final int resID) {
+        if (loop1IsRecording) {
+            Long time = System.currentTimeMillis() - loop1EndOfLastSound;
+            loop1PauseQueue.add(time);
+            loop1EndOfLastSound = System.currentTimeMillis();
+            spLoop1.load(context, resID, 0);
         }
 
-        ActionBar bar = getActionBar();
-        bar.setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+        if (loop2IsRecording) {
+            Long time = System.currentTimeMillis() - loop2EndOfLastSound;
+            loop2PauseQueue.add(time);
+            loop2EndOfLastSound = System.currentTimeMillis();
+            spLoop2.load(context, resID, 0);
+        }
 
-        return super.onOptionsItemSelected(item);
+        if (loop3IsRecording) {
+            Long time = System.currentTimeMillis() - loop3EndOfLastSound;
+            loop3PauseQueue.add(time);
+            loop3EndOfLastSound = System.currentTimeMillis();
+            spLoop3.load(context, resID, 0);
+        }
+    }
+
+    // Stop playing the loops if the user exits the app
+    @Override
+    public void onPause() {
+        super.onPause();
+        //loopTask.cancel(true);
+        loop1SoundQueue.clear();
+        loop1PauseQueue.clear();
+
+        //loopTask2.cancel(true);
+        loop2SoundQueue.clear();
+        loop2PauseQueue.clear();
+
+        //loopTask3.cancel(true);
+        loop3SoundQueue.clear();
+        loop3PauseQueue.clear();
+
+        loop1IsOn = false;
+        loop2IsOn = false;
+        loop3IsOn = false;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        ActionBar bar = getActionBar();
+//        bar.setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    public void releaseEverything() {
+//        spNormal.release();
+//        spHold1.release();
+//        spHold2.release();
+//        spHold3.release();
+
+//        // Destroy background threads when moving to new preset
+//        loopTask.cancel(true);
+//        loopTask2.cancel(true);
+//        loopTask3.cancel(true);
+
+        loop1IsRecording = false;
+        loop2IsRecording = false;
+        loop3IsRecording = false;
+
+        loop1IsOn = false;
+        loop2IsOn = false;
+        loop3IsOn = false;
+
+        spLoop1.release();
+        spLoop2.release();
+        spLoop3.release();
+
+        loop1SoundQueue.clear();
+        loop2SoundQueue.clear();
+        loop3SoundQueue.clear();
+
+        loop1PauseQueue.clear();
+        loop2PauseQueue.clear();
+        loop3PauseQueue.clear();
+    }
+
+    public void setUpHoldDownButton(final Button soundButton, final int sound, final SoundPool sp) {
+        soundButton.setSoundEffectsEnabled(false);
+        soundButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        soundButton.setPressed(true);
+                        sp.play(sound, 1, 1, 0, 0, 1);
+                    }
+                    break;
+
+                    case MotionEvent.ACTION_UP: {
+                        sp.stop(sp.play(sound, 1, 1, 0, 0, 1));
+                        soundButton.setPressed(false);
+                    }
+                    break;
+                }
+                return true;
+            }
+        });
+    }
+
+    // Adds on load listeners to the looping sound pools
+    public void setUpLoopSP(SoundPool spLoop, final Queue<Integer> soundQueue) {
+        spLoop.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                soundQueue.add(sampleId);
+                Log.d("hi owen", "Sound added");
+            }
+        });
+    }
+
+
+    public void runLoop1 () {
+        final Handler handler1 = new Handler();
+
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (loop1IsOn && !loop1SoundQueue.isEmpty()) {
+                    int soundID = loop1SoundQueue.remove();
+                    loop1SoundQueue.add(soundID);
+                    spLoop1.play(soundID, (float).9, (float).9, 0, 0, 1);
+
+                    // Pop the pause off the top and move it to the back of the queue
+                    Long pause = loop1PauseQueue.remove();
+                    loop1PauseQueue.add(pause);
+
+                    handler1.postDelayed(this, pause);
+                } else {
+                    handler1.removeCallbacks(this);
+                    loop1PauseQueue.clear();
+                    loop1SoundQueue.clear();
+                }
+
+            }
+        }, 0);
+    }
+
+    public void runLoop2 () {
+        final Handler handler2 = new Handler();
+
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (loop2IsOn && !loop2SoundQueue.isEmpty()) {
+                    int soundID = loop2SoundQueue.remove();
+                    loop2SoundQueue.add(soundID);
+                    spLoop2.play(soundID, (float).9, (float).9, 0, 0, 1);
+
+                    // Pop the pause off the top and move it to the back of the queue
+                    Long pause = loop2PauseQueue.remove();
+                    loop2PauseQueue.add(pause);
+
+                    handler2.postDelayed(this, pause);
+                } else {
+                    handler2.removeCallbacks(this);
+                    loop2SoundQueue.clear();
+                    loop2PauseQueue.clear();
+                }
+            }
+        }, 0);
+    }
+
+    public void runLoop3 () {
+        final Handler handler3 = new Handler();
+
+        handler3.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (loop3IsOn && !loop3SoundQueue.isEmpty()) {
+                    int soundID = loop3SoundQueue.remove();
+                    loop3SoundQueue.add(soundID);
+                    spLoop3.play(soundID, (float).9, (float).9, 0, 0, 1);
+
+                    // Pop the pause off the top and move it to the back of the queue
+                    Long pause = loop3PauseQueue.remove();
+                    loop3PauseQueue.add(pause);
+
+                    handler3.postDelayed(this, pause);
+                } else {
+                    handler3.removeCallbacks(this);
+                    loop3SoundQueue.clear();
+                    loop3PauseQueue.clear();
+                }
+            }
+        }, 0);
     }
 }
